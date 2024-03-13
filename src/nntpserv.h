0045346e1cc167c10b0fad6d7b06b3e7e55f6cce
axxisd <axxisd@gmail.com>
2024-03-13 18:52:06 -0500

    2.0.3 Add XHDR response for newsreaders that require it
      - Give a successsful response to clients that request XHDR and tell you
        you don't support it (no actual functionality, though)
      - Add MODE to help text recognized responses
      - Add XHDR to help text recognized responses

--- src/nntpserv.h
+++ src/nntpserv.h
@@ -69,8 +69,8 @@ struct var
 
 #define CRLF CR LF
 
-#define SERVER_NAME       "JamNNTPd/" PLATFORM_NAME
-#define SERVER_VERSION    "2.0.2"
+#define SERVER_NAME       "jamnntpd/" PLATFORM_NAME
+#define SERVER_VERSION    "2.0.3"
 #define SERVER_PIDVERSION SERVER_VERSION
 
 #define SOCKIO_TIMEOUT 5*60
