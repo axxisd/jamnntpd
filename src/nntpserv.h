639d93ed71d2944969850d0f699f09001aff9bc3
axxisd <axxisd@gmail.com>
2025-01-02 19:08:47 -0600

    2.4.9
      * nntpserv.c
        - Remove empty XHDR response to clients that request it (introduced
          in commit b9b0a38). The latest version of tin doesn't seem to play
          nice with it.
    
      * nntpserv.h
        - Minor version bump
    
      * Update CHANGELOG

--- src/nntpserv.h
+++ src/nntpserv.h
@@ -74,7 +74,7 @@ struct var
 #define CRLF CR LF
 
 #define SERVER_NAME       "JamNNTPd/" PLATFORM_NAME
-#define SERVER_VERSION    "2.4.8"
+#define SERVER_VERSION    "2.4.9"
 #define SERVER_PIDVERSION SERVER_VERSION
 
 #define SOCKIO_TIMEOUT 5*60
