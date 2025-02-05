ecfc942461810c1412c0f2ea5d95c752552ef399
axxisd <axxisd@gmail.com>
2025-02-04 19:33:45 -0600

    2.5.0
      * README
        - Minor spelling changes, update version and date.
    
      * nntpserv.h
        - Minor version bump
    
      * Update CHANGELOG

--- src/nntpserv.h
+++ src/nntpserv.h
@@ -74,7 +74,7 @@ struct var
 #define CRLF CR LF
 
 #define SERVER_NAME       "JamNNTPd/" PLATFORM_NAME
-#define SERVER_VERSION    "2.4.9"
+#define SERVER_VERSION    "2.5.0"
 #define SERVER_PIDVERSION SERVER_VERSION
 
 #define SOCKIO_TIMEOUT 5*60
